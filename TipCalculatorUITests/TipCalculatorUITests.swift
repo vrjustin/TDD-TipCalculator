e90abad3dda556bfd1ea4405934088706f22d10e
Justin Maronde <myemailisjustin@me.com>
2022-10-13 10:44:52 -0400

    * add a PageObject model layer between the tests for referencing the UI Elements. Now if we want to change the accessiblity identifiers in the actual contentView at a later time, we just need to adjust the PageObject values.

--- TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -10,29 +10,27 @@ import XCTest
 class when_ContentView_is_shown: XCTestCase {
     
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUpWithError() throws {
         app = XCUIApplication()
         continueAfterFailure = false
+        contentViewPage = ContentViewPage(app: app)
         app.launch()
     }
     
     override func tearDownWithError() throws {
         app = nil
+        contentViewPage = nil
     }
 
     func test_should_make_sure_totalTextField_shows_default_value() {
-
-        let totalTextField = app.textFields["totalTextField"]
-        
-        XCTAssertEqual(totalTextField.value as! String, "Enter total")
-        
+        XCTAssertEqual(contentViewPage.totalTextField.value as! String, "Enter total")
     }
     
     func test_percent_selection_starts_with_default_value_20() {
         
-        let segmentedControl = app.segmentedControls["tipPercentageSegmentedControl"]
-        let twentyPercentSegmentButton = segmentedControl.buttons.element(boundBy: 1)
+        let twentyPercentSegmentButton = contentViewPage.tipPercentageSegmentedControl.buttons.element(boundBy: 1)
         
         XCTAssertEqual(twentyPercentSegmentButton.label, "20%")
         XCTAssertTrue(twentyPercentSegmentButton.isSelected)
@@ -43,26 +41,29 @@ class when_ContentView_is_shown: XCTestCase {
 class when_calculate_button_is_pressed_for_valid_input: XCTestCase {
     
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUpWithError() throws {
         app = XCUIApplication()
         continueAfterFailure = false
+        contentViewPage = ContentViewPage(app: app)
         app.launch()
         
-        let totalTextField = app.textFields["totalTextField"]
+        let totalTextField = contentViewPage.totalTextField
         totalTextField.tap()
         totalTextField.typeText("100")
         
-        let calcuateTipButton = app.buttons["calculateTipButton"]
+        let calcuateTipButton = contentViewPage.calculateTipButton
         calcuateTipButton.tap()
     }
     
     override func tearDownWithError() throws {
         app = nil
+        contentViewPage = nil
     }
     
     func test_tip_is_shown_for_valid_input_amount() {
-        let tipText = app.staticTexts["tipTextValue"]
+        let tipText = contentViewPage.tipText
         let _ = tipText.waitForExistence(timeout: 0.5)
         XCTAssertEqual(tipText.label, "$20.00")
     }
@@ -72,33 +73,36 @@ class when_calculate_button_is_pressed_for_valid_input: XCTestCase {
 class when_calculate_button_is_pressed_for_Invalid_input: XCTestCase {
     
     private var app: XCUIApplication!
+    private var contentViewPage: ContentViewPage!
     
     override func setUpWithError() throws {
         app = XCUIApplication()
         continueAfterFailure = false
+        contentViewPage = ContentViewPage(app: app)
         app.launch()
         
-        let totalTextField = app.textFields["totalTextField"]
+        let totalTextField = contentViewPage.totalTextField
         totalTextField.tap()
         totalTextField.typeText("-100")
         
-        let calcuateTipButton = app.buttons["calculateTipButton"]
+        let calcuateTipButton = contentViewPage.calculateTipButton
         calcuateTipButton.tap()
     }
     
     override func tearDownWithError() throws {
         app = nil
+        contentViewPage = nil
     }
     
     func test_should_clear_tip_label_for_invalid_input() {
-        let tipText = app.staticTexts["tipTextValue"]
+        let tipText = contentViewPage.tipText
         let _ = tipText.waitForExistence(timeout: 0.5)
         XCTAssertEqual(tipText.label, "")
         
     }
     
     func test_error_message_is_shown_for_invalid_input() {
-        let messageText = app.staticTexts["messageLabelText"]
+        let messageText = contentViewPage.messageText
         let _ = messageText.waitForExistence(timeout: 0.5)
         XCTAssertEqual(messageText.label, "Invalid Input")
     }
