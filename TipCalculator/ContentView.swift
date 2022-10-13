f3b78f2d8feab9495e9f0a69e3e1cc7145d326f8
Justin Maronde <myemailisjustin@me.com>
2022-10-13 07:51:38 -0400

    initial commit from starting point

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var total: String = ""
+    @State private var tipPercentage: Double = 0.2
+    @State private var tip: String?
+    @State private var message: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            VStack {
+                
+                TextField("Enter total", text: $total)
+                    .textFieldStyle(.roundedBorder)
+                
+                Picker(selection: $tipPercentage) {
+                    Text("10%").tag(0.1)
+                    Text("20%").tag(0.2)
+                    Text("30%").tag(0.3)
+                } label: {
+                    EmptyView()
+                }.pickerStyle(.segmented)
+
+                
+                Button("Calculate Tip") {
+                  
+                    
+                }.padding(.top, 20)
+                
+                Text(message)
+                    .padding(.top, 50)
+                
+                Spacer()
+                
+                Text(tip ?? "")
+                    .font(.system(size: 54))
+                
+                Spacer()
+                .navigationTitle("Tip Calculator")
+            }.padding()
+            
+        }
     }
 }
 
